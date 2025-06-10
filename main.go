cda12b5014724960eb3efb26bb28be1df2b9fc10
rob121 <7796767+rob121@users.noreply.github.com>
2025-06-10 14:12:26 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,43 @@
+package nrpefn
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	OK       int = iota
+	Warning  int
+	Critical int
+)
+
+func FloatOK(value float64) {
+	fmt.Printf("OK - Value: %.2f\n", value)
+	os.Exit(OK)
+}
+
+func FloatWarning(value float64) {
+	fmt.Printf("WARNING - Value: %.2f\n", value)
+	os.Exit(Warning)
+}
+
+func FloatCritical(value float64) {
+	fmt.Printf("CRITICAL - Value: %.2f\n", value)
+	os.Exit(Critical)
+}
+
+func StringOK(value string) {
+	fmt.Printf("OK - %s\n", value)
+	os.Exit(OK)
+}
+
+func StringWarning(value string) {
+	fmt.Printf("WARNING - %s\n", value)
+	os.Exit(Warning)
+}
+
+func StringCritical(value string) {
+	fmt.Printf("CRITICAL - %s\n", value)
+	os.Exit(Critical)
+}
+
